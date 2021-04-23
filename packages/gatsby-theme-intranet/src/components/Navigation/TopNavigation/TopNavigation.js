1314fcb9cf959e3794cd4cd5b52e61a4e9a381fc
Younus <younus.abdalla@whitespace.se>
2021-04-23 15:04:56 +0200

    refactor(gatsby-theme-intranet): style top navigation

--- packages/gatsby-theme-intranet/src/components/Navigation/TopNavigation/TopNavigation.js
+++ packages/gatsby-theme-intranet/src/components/Navigation/TopNavigation/TopNavigation.js
@@ -5,6 +5,7 @@ import React from "react";
 import * as styles from "./TopNavigation.module.css";
 
 export function TopNavigation({ items, ...restProps }) {
+  console.log(items);
   return (
     <nav
       className={clsx(styles.component, "hidden-print")}
@@ -16,11 +17,11 @@ export function TopNavigation({ items, ...restProps }) {
           return (
             <li key={index} className={clsx(styles.item)}>
               <Link className={clsx(styles.link)} to={url} target={target}>
-                <Icon
+                {/* <Icon
                   name={icon}
                   className={clsx(styles.icon, `${styles.icon}--${icon}`)}
                   size="1.5rem"
-                />
+                /> */}
                 <div className={clsx(styles.text)}>
                   <span className={clsx(styles.label)}>{label}</span>
                 </div>
