9bf7fe0347f8cd152bdc464684deb63431264b71
Fredrik Johansson <jeanfredrik@gmail.com>
2021-04-26 09:38:15 +0200

    feat(gatsby-theme-basic): implement index display mode for posts module

---
+++ packages/gatsby-theme-basic/src/components/modularity-modules/posts-modules/IndexPostsModule.js
@@ -0,0 +1,52 @@
+import {
+  Link,
+  Time,
+} from "@whitespace/gatsby-theme-wordpress-basic/src/components";
+import clsx from "clsx";
+import React from "react";
+
+import Grid from "../../Grid";
+import ModuleWrapper from "../../ModuleWrapper";
+
+import * as defaultStyles from "./IndexPostsModule.module.css";
+
+export default function IndexPostsModule({
+  styles = defaultStyles,
+  className,
+  title,
+  module,
+  normalizedItems,
+  ...restProps
+}) {
+  const {
+    dataDisplay: { postsFields },
+  } = module;
+  let showDate = postsFields?.includes("date");
+
+  return (
+    <ModuleWrapper
+      title={title}
+      {...restProps}
+      className={clsx(styles.component, className)}
+    >
+      <Grid className={clsx(styles.list)}>
+        {normalizedItems.map((item, index) => {
+          return (
+            // TODO: Add <Card>
+            <div key={index} className={clsx(styles.item)}>
+              <Link to={item.url} className={clsx(styles.link)}>
+                <span>{item.title}</span>
+              </Link>{" "}
+              {!!showDate && (
+                <Time
+                  date={item.dateGmt}
+                  format={{ year: "numeric", month: "numeric", day: "numeric" }}
+                />
+              )}
+            </div>
+          );
+        })}
+      </Grid>
+    </ModuleWrapper>
+  );
+}
--- packages/gatsby-theme-basic/src/components/modularity-modules/posts-modules/index.js
+++ packages/gatsby-theme-basic/src/components/modularity-modules/posts-modules/index.js
@@ -1,2 +1,3 @@
 export { default as DefaultPostsModule } from "./DefaultPostsModule";
 export { default as ListPostsModule } from "./ListPostsModule";
+export { default as IndexPostsModule } from "./IndexPostsModule";
