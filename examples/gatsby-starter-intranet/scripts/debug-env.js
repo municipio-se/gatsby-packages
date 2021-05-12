750002bcd66b9f5a703e11801bb7ff0ad33c967d
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-12 16:07:50 +0200

    chore(docs): remove gatsby-starter-intranet

--- examples/gatsby-starter-intranet/scripts/debug-env.js
+++
@@ -1,5 +0,0 @@
-const { loadConfig } = require("@municipio/gatsby-theme-basic");
-
-loadConfig();
-
-console.info(process.env);
--- examples/gatsby-starter-intranet/src/fragments/WP_ContentNodeModularityAreas.js
+++
@@ -1,9 +0,0 @@
-import { graphql } from "gatsby";
-
-export const query = graphql`
-  fragment WP_ContentNodeModularityAreas on WP_NodeWithModularity {
-    contentArea: modularityArea(area: CONTENT_AREA) {
-      ...WP_ModularityAreaModules
-    }
-  }
-`;
