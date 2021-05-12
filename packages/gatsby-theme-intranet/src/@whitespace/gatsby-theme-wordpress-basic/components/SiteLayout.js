382ddd25a4c7048124f0dfbeff021dbd9a3d454f
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-12 17:00:59 +0200

    chore(gatsby-theme-intranet)!: update gatsby-theme-basic

--- packages/gatsby-theme-intranet/src/@whitespace/gatsby-theme-wordpress-basic/components/SiteLayout.js
+++ packages/gatsby-theme-intranet/src/@whitespace/gatsby-theme-wordpress-basic/components/SiteLayout.js
@@ -1,15 +1,18 @@
-import { PageContextProvider } from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/page-context";
 import cx from "classnames";
 // import useLocation from "gatsby-theme-municipio/src/hooks/location";
 // import usePrevious from "gatsby-theme-municipio/src/hooks/previous";
-import React, { createContext, useState, useEffect } from "react";
+import React, {
+  createContext,
+  useState,
+  // useEffect
+} from "react";
 
 import Sidebar from "../../../components/Sidebar";
 import Toolbar from "../../../components/Toolbar";
 
 import * as styles from "./SiteLayout.module.css";
 
-export default function SiteLayout({ pageContext, children }) {
+export default function SiteLayout({ children }) {
   const [siteContext, setSiteContext] = useState({ menuOpen: false });
   // const location = useLocation();
   // const prevLocation = usePrevious(location);
@@ -24,22 +27,17 @@ export default function SiteLayout({ pageContext, children }) {
   // }, [location, prevLocation, setSiteContext]);
 
   return (
-    <PageContextProvider value={pageContext}>
-      <SiteLayoutContext.Provider value={[siteContext, setSiteContext]}>
-        <div
-          className={cx(
-            styles.component,
-            siteContext.menuOpen && "sidebar-open",
-          )}
-        >
-          <Sidebar />
-          <main className={styles.main} id="main">
-            <Toolbar />
-            <div className={styles.content}>{children}</div>
-          </main>
-        </div>
-      </SiteLayoutContext.Provider>
-    </PageContextProvider>
+    <SiteLayoutContext.Provider value={[siteContext, setSiteContext]}>
+      <div
+        className={cx(styles.component, siteContext.menuOpen && "sidebar-open")}
+      >
+        <Sidebar />
+        <main className={styles.main} id="main">
+          <Toolbar />
+          <div className={styles.content}>{children}</div>
+        </main>
+      </div>
+    </SiteLayoutContext.Provider>
   );
 }
 
