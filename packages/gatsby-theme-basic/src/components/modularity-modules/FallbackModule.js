9156a18770c91db335843430027f2b1203e9a65e
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-12 11:42:24 +0200

    feat(gatsby-theme-basic): wrap fallback module like other modules

--- packages/gatsby-theme-basic/src/components/modularity-modules/FallbackModule.js
+++ packages/gatsby-theme-basic/src/components/modularity-modules/FallbackModule.js
@@ -1,18 +1,25 @@
 import React from "react";
 
+import ModuleWrapper from "../ModuleWrapper";
+
 export default function FallbackModule({ module }) {
+  // Don’t render this component in production
   if (process.env.NODE_ENV === "production") {
     return null;
   }
+
   const moduleType = module?.contentType?.node?.name;
+
   return (
-    <details>
-      <summary>
-        <code>{`Unimplemented <${moduleType}/>`}</code>
-      </summary>
-      <pre>
-        <code>{JSON.stringify(module, null, 2)}</code>
-      </pre>
-    </details>
+    <ModuleWrapper>
+      <details>
+        <summary>
+          <code>{`Unimplemented <${moduleType}/>`}</code>
+        </summary>
+        <pre>
+          <code>{JSON.stringify(module, null, 2)}</code>
+        </pre>
+      </details>
+    </ModuleWrapper>
   );
 }
