d2fe78446d73a5a4575bdd2f9309c91aa4181e09
Younus <younus.abdalla@whitespace.se>
2021-06-15 16:58:14 +0200

    feat: implement modiframe

---
+++ packages/gatsby-theme-basic/src/components/modularity-modules/IframeModule.js
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import React from "react";
+
+import ModuleWrapper from "../ModuleWrapper";
+
+import * as defaultStyles from "./IframeModule.module.css";
+
+export default function IframeModule({
+  styles = defaultStyles,
+  className,
+  title,
+  module = {},
+  ...restProps
+}) {
+  const { modIframeOptions: { iframeHeight, iframeUrl } = {} } = module;
+
+  return (
+    <ModuleWrapper
+      title={title}
+      {...restProps}
+      className={clsx(styles.component, className)}
+    >
+      <iframe src={iframeUrl} height={iframeHeight} width="100%" frameBorder={0} />
+    </ModuleWrapper>
+  );
+}
--- packages/gatsby-theme-basic/src/components/modularity-modules/index.js
+++ packages/gatsby-theme-basic/src/components/modularity-modules/index.js
@@ -4,6 +4,7 @@ export { default as FallbackModule } from "./FallbackModule";
 export { default as FileslistModule } from "./FileslistModule";
 export { default as FormModule } from "./FormModule";
 export { default as GalleryModule } from "./GalleryModule";
+export { default as IframeModule } from "./IframeModule";
 export { default as ImageModule } from "./ImageModule";
 export { default as NoticeModule } from "./NoticeModule";
 export { default as PostsModule } from "./PostsModule";
