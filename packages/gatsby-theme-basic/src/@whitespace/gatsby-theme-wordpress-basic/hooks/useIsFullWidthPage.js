49ffc1c53b6a8636753234701426d9288ed43a58
Cynthia Dao <cynthiamicheld@gmail.com>
2021-06-16 15:16:29 +0000

    style: auto-format all files [skip ci]

--- packages/gatsby-theme-basic/src/@whitespace/gatsby-theme-wordpress-basic/hooks/useIsFullWidthPage.js
+++ packages/gatsby-theme-basic/src/@whitespace/gatsby-theme-wordpress-basic/hooks/useIsFullWidthPage.js
@@ -1,15 +1,10 @@
 import { usePageContext } from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/page-context";
-import useIsFullWidthPageParent  from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/useIsFullWidthPage";
-
-
+import useIsFullWidthPageParent from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/useIsFullWidthPage";
 
 export default function useIsFullWidthPage() {
   const {
-    contentNode: {
-      pageAppearance: { template } = {}
-    }
-  }
-    = usePageContext();
-  
+    contentNode: { pageAppearance: { template } = {} },
+  } = usePageContext();
+
   return useIsFullWidthPageParent() || template == "landingPage";
 }
