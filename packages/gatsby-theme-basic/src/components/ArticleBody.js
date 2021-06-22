e138e2c643a89934a17d7892f48a16a6a1e23743
Fredrik Johansson <jeanfredrik@gmail.com>
2021-06-22 12:33:13 +0200

    fix: broken styling of article preamble

--- packages/gatsby-theme-basic/src/components/ArticleBody.js
+++ packages/gatsby-theme-basic/src/components/ArticleBody.js
@@ -1,3 +1,4 @@
+import * as defaultStyles from "@whitespace/gatsby-theme-wordpress-basic/src/components/ArticleBody.module.css";
 import TextContent from "@whitespace/gatsby-theme-wordpress-basic/src/components/TextContent";
 import WPBlocks from "@whitespace/gatsby-theme-wordpress-basic/src/components/WPBlocks";
 import {
@@ -5,19 +6,17 @@ import {
   usePageContext,
 } from "@whitespace/gatsby-theme-wordpress-basic/src/hooks";
 import clsx from "clsx";
+import PropTypes from "prop-types";
 import React from "react";
 
 import ModularityArea from "./ModularityArea";
 
-// import * as defaultStyles from "./ArticleBody.module.css";
+ArticleBody.propTypes = {
+  styles: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+};
 
-ArticleBody.propTypes = {};
-
-export default function ArticleBody({
-  // styles = defaultStyles,
-  styles = {},
-  ...restProps
-}) {
+export default function ArticleBody({ styles = defaultStyles, ...restProps }) {
   let {
     contentNode: {
       blocksJSON,
