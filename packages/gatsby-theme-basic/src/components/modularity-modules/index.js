5b128b878484860eac5babaf9535ef7cf059c85e
Fredrik Johansson <jeanfredrik@gmail.com>
2021-08-23 12:52:14 +0200

    chore: remove broken code

--- packages/gatsby-theme-basic/src/components/modularity-modules/index.js
+++ packages/gatsby-theme-basic/src/components/modularity-modules/index.js
@@ -2,7 +2,6 @@ export { default as ColorBlocksModule } from "./ColorBlocksModule";
 export { default as ContactsModule } from "./ContactsModule";
 export { default as FallbackModule } from "./FallbackModule";
 export { default as FileslistModule } from "./FileslistModule";
-export { default as FormModule } from "./FormModule";
 export { default as GalleryModule } from "./GalleryModule";
 export { default as IframeModule } from "./IframeModule";
 export { default as ImageModule } from "./ImageModule";
