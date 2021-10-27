e7ec4b064007be532cca1fc3da582833ef704dea
Mattias Hinderson <mattias@hinderson.com>
2021-10-27 15:37:05 +0200

    feat: implement Gallery Module

--- packages/gatsby-theme-basic/src/components/modularity-modules/index.js
+++ packages/gatsby-theme-basic/src/components/modularity-modules/index.js
@@ -1,6 +1,7 @@
 export { default as ContactsModule } from "./ContactsModule";
 export { default as FallbackModule } from "./FallbackModule";
 export { default as FileslistModule } from "./FileslistModule";
+export { default as GalleryModule } from "./GalleryModule";
 export { default as IframeModule } from "./IframeModule";
 export { default as ImageModule } from "./ImageModule";
 export { default as MenuModule } from "./MenuModule";
