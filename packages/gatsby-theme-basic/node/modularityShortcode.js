cb152e594a2e348c201d60405ea5e95a1c00296b
Fredrik Johansson <jeanfredrik@gmail.com>
2022-10-18 18:19:13 +0200

    chore: Clean up code

--- packages/gatsby-theme-basic/node/modularityShortcode.js
+++ packages/gatsby-theme-basic/node/modularityShortcode.js
@@ -1,26 +1,8 @@
 import WPModularityModule from "../src/components/WPModularityModule.js";
 
 export default {
-  inputTransforms: [
-    // (input) => {
-    //   input.replace(/\[modularity\b([^\]]*)\]/g, "<modularity$1></modularity>");
-    // },
-  ],
-  treeTransforms: [
-    // (tree, { visit, contentModularityModules }) => {
-    //   // visit(tree, 'modularity', (node, index, parent) => {
-    //   //   node.tagName = 'div';
-    //   //   node.properties = {
-    //   //     ...node.properties,
-    //   //     className: [
-    //   //       ...(node.properties && node.properties.className) || [],
-    //   //       'modularity',
-    //   //     ],
-    //   //   };
-    //   //   return [visit.SKIP, index + 1];
-    //   // });
-    // },
-  ],
+  inputTransforms: [],
+  treeTransforms: [],
   stringifierComponents: {
     modularity: WPModularityModule,
   },
