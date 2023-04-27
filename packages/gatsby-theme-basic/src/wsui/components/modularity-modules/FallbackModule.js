285931909e57c2f2a2e379ba1683cf8422f26815
Fredrik Johansson <jeanfredrik@gmail.com>
2023-04-27 14:34:36 +0200

    feat: Improve fallback module

--- packages/gatsby-theme-basic/src/wsui/components/modularity-modules/FallbackModule.js
+++ packages/gatsby-theme-basic/src/wsui/components/modularity-modules/FallbackModule.js
@@ -1,3 +1,4 @@
+import { Box } from "@wsui/base";
 import React from "react";
 
 import ModuleWrapper from "../ModuleWrapper.jsx";
@@ -8,14 +9,16 @@ export default function FallbackModule({ module, title, ...restProps }) {
   return (
     <ModuleWrapper title={title} {...restProps}>
       {process.env.NODE_ENV !== "production" && (
-        <details>
-          <summary>
-            <code>{`Unimplemented <${moduleType}/>`}</code>
-          </summary>
-          <pre>
-            <code>{JSON.stringify(module, null, 2)}</code>
-          </pre>
-        </details>
+        <Box>
+          <details>
+            <summary>
+              <code>{`Unimplemented <${moduleType}/>`}</code>
+            </summary>
+            <pre>
+              <code>{JSON.stringify(module, null, 2)}</code>
+            </pre>
+          </details>
+        </Box>
       )}
     </ModuleWrapper>
   );
