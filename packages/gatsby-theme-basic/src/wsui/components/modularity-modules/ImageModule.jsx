b2970876231dc01373cafe8b1ee0ae76256c6da0
Fredrik Johansson <jeanfredrik@gmail.com>
2023-05-02 15:21:32 +0200

    fix: Handle broken image modules

--- packages/gatsby-theme-basic/src/wsui/components/modularity-modules/ImageModule.jsx
+++ packages/gatsby-theme-basic/src/wsui/components/modularity-modules/ImageModule.jsx
@@ -7,33 +7,33 @@ import React from "react";
 import ModuleWrapper from "../ModuleWrapper.jsx";
 
 export default function ImageModule({ module = {}, title, ...restProps }) {
-  const {
-    modImageOptions: {
-      modImageImage: {
-        alt,
-        base64,
-        caption,
-        credit,
-        height,
-        src,
-        srcSet,
-        srcSetWebp,
-        width,
-      } = {},
-      link,
-      modImageCaption,
-    } = {},
-  } = module;
+  let modImageOptions = module?.modImageOptions;
+  let modImageImage = modImageOptions?.modImageImage;
+
+  if (!modImageImage) return null;
+
+  let caption = modImageImage?.caption || modImageOptions?.modImageCaption;
+  let alt = modImageImage?.alt;
+  let base64 = modImageImage?.base64;
+  let credit = modImageImage?.credit;
+  let height = modImageImage?.height;
+  let linkTo = modImageImage?.link?.url;
+  let src = modImageImage?.src;
+  let srcSet = modImageImage?.srcSet;
+  let srcSetWebp = modImageImage?.srcSetWebp;
+  let width = modImageImage?.width;
+
+  if (!src) return null;
 
   return (
     <ModuleWrapper title={title} {...restProps}>
       <Image
         alt={alt}
         base64={base64}
-        caption={<TextContent>{modImageCaption || caption}</TextContent>}
+        caption={<TextContent>{caption}</TextContent>}
         credit={credit}
         height={height}
-        linkTo={link?.url}
+        linkTo={linkTo}
         src={src}
         srcSet={srcSet}
         srcSetWebp={srcSetWebp}
