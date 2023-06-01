c420ad65df70670a16257826383f3cf7fb10b582
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2023-06-01 10:06:53 +0200

    Added permalink as url to the postsModule

--- packages/gatsby-theme-basic/src/wsui/components/modularity-modules/PostsModule.jsx
+++ packages/gatsby-theme-basic/src/wsui/components/modularity-modules/PostsModule.jsx
@@ -27,6 +27,7 @@ function defaultNormalizePostsModuleItems(
     case "input":
       return (modPostsDataSource.data || []).map(
         ({ postContentMedia, postContentModularityModules, ...item }) => {
+          let permalink = "/" + item?.permalink?.split("/").slice(3).join("/");
           let processedContent = (
             <HTML
               contentMedia={postContentMedia}
@@ -40,6 +41,7 @@ function defaultNormalizePostsModuleItems(
             ...item,
             title: item.postTitle,
             link: item?.link,
+            url: permalink,
             description: stripHTML(item.postContent),
             content: processedContent,
           };
