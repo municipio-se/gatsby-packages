94b1839cf2326b02261695e5ced96eea63a6b0e1
Fredrik Johansson <jeanfredrik@gmail.com>
2023-06-29 13:55:43 +0200

    fix: Text module title and content was not handled properly

--- packages/gatsby-theme-basic/src/wsui/components/modularity-modules/TextModule.jsx
+++ packages/gatsby-theme-basic/src/wsui/components/modularity-modules/TextModule.jsx
@@ -1,3 +1,4 @@
+import { useHTMLProcessor } from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/html-processor";
 import { useThemeProps } from "@wsui/base";
 import { TextModule as WsuiTextModule } from "@wsui/municipio";
 import React, { useContext } from "react";
@@ -9,20 +10,36 @@ import modularityRowContext from "../../../modularityRowContext";
 export default function TextModule(props) {
   props = useThemeProps({ props, name: "MunicipioTextModule" });
   let {
-    title,
+    // eslint-disable-next-line no-unused-vars
+    title: ignoredTitle,
+    // eslint-disable-next-line no-unused-vars
     description,
-    module: {
-      modTextOptions: {
-        hideBoxFrame = false,
-        theme: color,
-        link,
-        icon,
-        displayMode,
-        backgroundImage,
-      },
-    },
+    module,
     ...restProps
   } = props;
+
+  let {
+    title,
+    hideTitle,
+    modTextOptions: {
+      hideBoxFrame = false,
+      theme: color,
+      link,
+      icon,
+      displayMode,
+      backgroundImage,
+    },
+  } = module;
+
+  const { processPageContent } = useHTMLProcessor();
+  // TODO: Deprecate `modDescription` in favor of `content`
+  let { content } = processPageContent(module.content, {
+    extractHeading: false,
+    leavePreamble: true,
+    contentMedia: module.contentMedia,
+    contentModularityModules: module.contentModularityModules,
+    semanticHeadings: true,
+  });
 
   const { centerLonelyModules } = useContext(modularityAreaContext);
   const { modules } = useContext(modularityRowContext);
@@ -36,8 +53,8 @@ export default function TextModule(props) {
   return (
     <WsuiTextModule
       color={(!hideBoxFrame && color) || undefined}
-      title={title}
-      content={description}
+      title={hideTitle ? undefined : title}
+      content={content}
       link={link}
       icon={icon}
       displayMode={displayMode}
