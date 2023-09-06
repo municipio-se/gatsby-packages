238bc2a46d08894a3f9cc2f09f56e9fbb3ca183d
Fredrik Johansson <jeanfredrik@gmail.com>
2023-09-06 18:08:10 +0200

    refactor!: Adapt to wsui text module changes

--- packages/gatsby-theme-basic/src/wsui/components/modularity-modules/TextModule.jsx
+++ packages/gatsby-theme-basic/src/wsui/components/modularity-modules/TextModule.jsx
@@ -1,25 +1,16 @@
-import { useHTMLProcessor } from "@whitespace/gatsby-theme-wordpress-basic/src/hooks/html-processor";
 import { useThemeProps } from "@wsui/base";
-import { TextModule as WsuiTextModule } from "@wsui/municipio";
+import { TextModuleController } from "@wsui/municipio";
 import React, { useContext } from "react";
 
 import modularityModuleContext from "../../../modularityModuleContext";
 
 export default function TextModule(props) {
   props = useThemeProps({ props, name: "MunicipioTextModule" });
-  let {
-    // eslint-disable-next-line no-unused-vars
-    title: ignoredTitle,
-    // eslint-disable-next-line no-unused-vars
-    description,
-    module,
-    ...restProps
-  } = props;
+  let { module, ...restProps } = props;
 
   let {
-    title,
-    hideTitle,
     modTextOptions: {
+      expandable,
       hideBoxFrame = false,
       theme: color,
       link, // TODO: Remove this since it's no longer used
@@ -30,29 +21,18 @@ export default function TextModule(props) {
     },
   } = module;
 
-  const { processPageContent } = useHTMLProcessor();
-  // TODO: Deprecate `modDescription` in favor of `content`
-  let { content } = processPageContent(module.content, {
-    extractHeading: false,
-    leavePreamble: true,
-    contentMedia: module.contentMedia,
-    contentModularityModules: module.contentModularityModules,
-    semanticHeadings: true,
-  });
-
   let { align } = useContext(modularityModuleContext);
 
   return (
-    <WsuiTextModule
+    <TextModuleController
       color={(!hideBoxFrame && color) || undefined}
-      title={hideTitle ? undefined : title}
-      content={content}
       link={link}
       icon={icon}
       displayMode={displayMode}
       align={align}
       backgroundImage={backgroundImage}
       links={links}
+      expandable={expandable}
       {...restProps}
     />
   );
